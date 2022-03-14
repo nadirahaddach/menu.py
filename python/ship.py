98cc27ddedbfb6663c5f9b607ee6385623fdb330
nadira :D <89223568+nadirahaddach@users.noreply.github.com>
2022-03-14 19:10:04 +0000

    Initial commit

---
+++ python/ship.py
@@ -0,0 +1,41 @@
+import time
+import os
+
+Color34 = "\u001b[34m"
+Color37 = "\u001b[37m"
+
+
+# As you can see, its not very optimal
+def ship1():
+    print("    |\ ")
+    print("    |/ ")
+    print("\__ |__/ ")
+    print(" \____/ ")
+    print("\u001b[34m -------------------------------------------- \u001b[37m")
+  
+  
+def ship2():
+    print("      |\ ")
+    print("      |/ ")
+    print("  \__ |__/ ")
+    print("   \____/ ")
+    print("\u001b[34m ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ \u001b[37m")
+  
+  
+def ship3():
+    print("        |\ ")
+    print("        |/ ")
+    print("    \__ |__/ ")
+    print("     \____/ ")
+    print("\u001b[34m ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ \u001b[37m")
+  
+  
+def ship4():
+    print("          |\ ")
+    print("          |/ ")
+    print("      \__ |__/ ")
+    print("       \____/ ")
+    print("\u001b[34m ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ \u001b[37m")
+  
+  
+   #     time.sleep(.1)
